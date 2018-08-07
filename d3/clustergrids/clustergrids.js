56bd4416acff4f51f4a58fd36912edf70c2b9a2e
Mark Myslin <mark@ravellaw.com>
2018-08-06 18:51:05 -0700

    cluster grids

---
+++ d3/clustergrids/clustergrids.js
@@ -0,0 +1,71 @@
+
+var size = 200
+var nrow = Math.trunc(Math.sqrt(size))
+var mainCluster = Array(nrow)
+var leftoverSize = size - nrow*nrow
+var addToBottom = Array(leftoverSize).slice(0, nrow)
+var addToSide = Array(leftoverSize-addToBottom.length)
+
+var svgWidth = 500, svgHeight = 500
+var radius = 4
+var margin = radius*2
+
+var grid = d3.select("#grid")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+        .selectAll("g")
+    .data(mainCluster)
+    .enter();
+    
+// Build main cluster
+
+    grid
+     .append("g")
+    .attr("transform", function (d, i) {
+      return "translate(" + [0,(2*radius+margin)*i] + ")" 
+    })
+    .selectAll("circle")
+    .data(mainCluster)
+    .enter()
+    .append("circle")
+    .attr("transform", function (d, i) {
+      return "translate(" + [margin+(2*radius+margin)*i,0] + ")" 
+    })
+    .attr("r", radius)
+    .attr("cx", radius)
+    .attr("cy", radius);
+
+    // Append leftovers to bottom
+
+    grid
+         .append("g")
+    .selectAll("circle")
+    .data(addToBottom)
+    .enter()
+    .append("circle")
+    .attr("transform", function (d, i) {
+      return "translate(" + [margin+(2*radius+margin)*i,(2*radius+margin)*nrow] + ")" 
+    })
+    .attr("r", radius)
+    .attr("cx", radius)
+    .attr("fill", "blue")
+    .attr("cy", radius);
+
+    
+ // Append leftovers to side
+
+    grid
+         .append("g")
+    .selectAll("circle")
+    .data(addToSide)
+    .enter()
+    .append("circle")
+    .attr("transform", function (d, i) {
+      return "translate(" + [margin+(2*radius+margin)*nrow, (2*radius+margin)*i] + ")" 
+    })
+    .attr("r", radius)
+    .attr("cx", radius)
+    .attr("fill", "red")
+    .attr("cy", radius);
+
+    
--- d3/venndiagram/venndiagram.js
+++ d3/venndiagram/venndiagram.js
@@ -1,67 +1,39 @@
 
-var colors = [
-    [ 39, 55, 5, 34 ],
-    [ 30, 2, 27 , 3],
-    [ 55, 5, 61, 76 ],
-    [ 55, 47, 94, 11 ],
-    [ 50, 37, 97, 11 ],
-    [ 55, 0, 0, 14]
-];
-
-var rows = colors.length;
-var cols = colors[0].length;
-var margin = 1;
-var cellWidth = 50;
-var cellHeight = 30;
 
 
-var grid = d3.select("#grid")
-    .attr("width", cols * (cellWidth + margin) )
-    .attr("height", rows * (cellHeight + margin) )
-    .selectAll("g")
-    .data(colors)
-    .enter();
+var xcenters = [33,66,50];
+var ycenters = [33,33,50];
+var colors = ["red","green","blue"];
+var radius = 30;
+
+var svgWidth = 700, svgHeight = 700;
+
+
+var yScale = d3.scaleLinear()
+.domain([0,100])
+.range([0, svgHeight]);
+
+var xScale = d3.scaleLinear()
+.domain([0,100])
+.range([0, svgWidth]);
+
+
+var x_axis = d3.axisBottom()//.scale(xScale);
+var y_axis = d3.axisLeft().scale(yScale);
+
+var venndiagrama = d3.select("#venndiagram")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .selectAll("circle")
+    .data(xcenters)
+    .enter()
+    .append("circle")
+    .attr("r", xScale(radius))
+    .attr("cx", function(d){return xScale(d)})
+    .attr("cy", function(d,i){return yScale(ycenters[i])})
+    .attr("fill-opacity", 0.2)
+    .attr("fill", function(d,i){return colors[i]});
+
+     venndiagrama.append("g")
+       .call(x_axis);
     
-    grid
-    .append("g")
-    .attr("transform", function (d, i) {
-      return "translate(" + [0,i*(cellHeight+margin)] + ")" 
-    })
-    .selectAll("rect")
-    .data(function(d) {return d;})
-    .enter().append("rect")
-    .attr("transform", function (d, c) {
-      return "translate(" + [c*(cellWidth+margin),0] + ")" 
-    })
-    .attr("height", cellHeight)
-    .attr("width", cellWidth)
-    .attr("fill", "#276392")
-    .attr("fill-opacity", function (d,i) {
-      return (d+1)/100;
-    })
-    ;
-
-   grid
-    .append("g")
-    .attr("transform", function (d, i) {
-      return "translate(" + [0,cellHeight/2 +cellHeight/4 + i*(cellHeight+margin)] + ")" 
-    })
-    .selectAll("text")
-    .data(function(d) {return d;})
-    .enter().append("text")
-    .attr("text-anchor", "middle")
-    .attr("alignment-baseline", "central")
-    .attr("font-family", "TitilliumText25L")
-    .attr("font-weight", 400)
-    .attr("font-size", cellHeight/2)
-    .attr("transform", function (d, c) {
-      return "translate(" + [cellWidth/2 + c*(cellWidth+margin),0] + ")" 
-    })
-.text(function(d) {
-  return d;
-})
-    ;
-
-
-
-
