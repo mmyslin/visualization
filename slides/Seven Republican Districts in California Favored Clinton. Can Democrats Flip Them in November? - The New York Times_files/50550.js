221af8657a9d479858e4d89b6afe76878befce74
Mark Myslin <mark@ravellaw.com>
2018-08-30 19:00:32 -0700

    scrolly

---
+++ slides/Seven Republican Districts in California Favored Clinton. Can Democrats Flip Them in November? - The New York Times_files/50550.js
@@ -0,0 +1,156 @@
+var bk_results = {
+ "campaigns": [
+  {
+    "campaign": 221329,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1024818,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 216266,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "wf2uCOHI0XkQyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 997861,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 221924,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "Qn4pCJW7nXoQyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1021905,
+     "timestamp": 1535679029
+    }
+    ]
+  },
+  {
+    "campaign": 260007,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "p619CKKH9H4QyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1046073,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 245967,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "UUZpCLzWuXwQyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 997861,
+     "timestamp": 1535678993
+    },
+    {
+     "categoryID": 1146150,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 221236,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1046073,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 218185,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 997861,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 221252,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1024818,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 231846,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1052731,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 221922,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1021905,
+     "timestamp": 1535679029
+    }
+    ]
+  },
+  {
+    "campaign": 231853,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "JzHrCIa2jXsQyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1052731,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 284641,
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 997861,
+     "timestamp": 1535678993
+    }
+    ]
+  },
+  {
+    "campaign": 228278,
+    "amp;guid": "ON",
+    "amp;script": "0",
+    "label": "IsVWCMvUln4QyL734AM",
+    "timestamp": 1535679047,
+    "categories": [
+    {
+     "categoryID": 1024818,
+     "timestamp": 1535678993
+    }
+    ]
+  }
+ ]
+};
---
+++ slides/Seven Republican Districts in California Favored Clinton. Can Democrats Flip Them in November? - The New York Times_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "CLEVELAND",
+      'continent': "NA",
+      'country': "US",
+      'region': "OH"
+    },
+    'ip':"198.176.81.34"
+  }]);
+})
+//
+()
+
+;
